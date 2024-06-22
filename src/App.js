7ec5ca2a9a84a2f0569df644024a7cb84c9ee045
mainguyen2202 <trucmainguyen02@gmail.com>
2024-06-22 17:29:48 +0700

    undefined, xóa React.StrictMode

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 // import './App.css';
+import React, { useEffect } from "react";
 import Footer from './components/headerFooter/Footer';
 import Header from './components/headerFooter/Header';
-import { Outlet } from "react-router-dom";
-import { useEffect } from 'react';
-import { toast, ToastContainer, Zoom } from 'react-toastify';
+
+import { ToastContainer, Zoom } from 'react-toastify';
+
 import 'react-toastify/dist/ReactToastify.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/homes/Home';
+import About from './components/homes/About';
+import Places from './components/place/Places';
+import Contact from './components/homes/Contact';
+import Like from './components/homes/Like';
+import PlacesSingle from './components/place/PlacesSingle';
+import Itinerarie from './components/Itinerarie/Itinerarie';
+import ItinerarieView from './components/Itinerarie/ItinerarieView';
+import Login from './components/Profile/Login';
+import Registration from './components/Profile/Registration';
+import ForgotPassword from './components/Profile/ForgotPassword';
+import Profile from './components/Profile/Profile';
+// import ChangePassword from './components/Profile/ChangePassword';
+import ResetPassword from './components/Profile/ResetPassword';
+import { ACCESS_TOKEN } from "./constants/constants";
+import { getCurrentUser } from "./services/authServices";
+
 
 function App() {
+
+
 
 	useEffect(() => {
 		console.log("app");
 		console.log("key", process.env.REACT_APP_GOOGLE_MAPS_KEY);
-	  }, []);
+	}, []);
 
 	return (
+
 		<div className="App">
 			<div className='app-header'>
 				<Header />
 			</div>
 			<div className='app-content'>
 				{/* để phần con của nó */}
-				<Outlet />
+				{/* <Outlet /> */}
+				{/* <BrowserRouter> */}
+					<Routes>
+
+
+						{/* index route để nó mặc định trang đầu */}
+						{/* <Route index element={<Home />} /> */}
+						<Route path="/" element={<Home />} />
+						<Route path="about" element={<About />} />
+						<Route path="places" element={<Places />} />
+
+						{/*  path - endpoint có tham số route param */}
+						{/* <Route path="detail/:placeId" element={<PlacesSingle/>} />   */}
+						<Route path="detail" element={<PlacesSingle />} />
+
+						{/* // route param */}
+
+						<Route path="itinerarie" element={<Itinerarie />} />
+						<Route path="itinerarieView" element={<ItinerarieView />} />
+						<Route path="like" element={<Like />} />
+
+						<Route path="contact" element={<Contact />} />
+
+
+
+
+						<Route path="login" element={<Login />} />
+						<Route path="registration" element={<Registration />} />
+
+						<Route path="profile" element={<Profile />} />
+						{/* <Route path="changePassword/:userId" element={<ChangePassword />} /> */}
+						<Route path="forgotPassword" element={<ForgotPassword />} />
+						<Route path="resetPassword" element={<ResetPassword />} />
+
+
+
+
+
+					</Routes>
+				{/* </BrowserRouter> */}
 				{/* app content */}
 			</div>
 			<div className='app-header'>
@@ -28,17 +89,19 @@ function App() {
 			</div>
 
 			<ToastContainer
-                                        className="toast-container"
-                                        toastClassName="toast"
-                                        bodyClassName="toast-body"
-                                        progressClassName="toast-progress"
-                                        theme='colored'
-                                        transition={Zoom}
-                                        autoClose={5}
-                                        hideProgressBar={true}
-                                    ></ToastContainer>
+				className="toast-container"
+				toastClassName="toast"
+				bodyClassName="toast-body"
+				progressClassName="toast-progress"
+				theme='colored'
+				transition={Zoom}
+				autoClose={5}
+				hideProgressBar={true}
+			></ToastContainer>
 
 		</div>
+
+
 	);
 }
 
--- src/index.js
+++ src/index.js
@@ -3,77 +3,17 @@ import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/homes/Home';
-import About from './components/homes/About';
-import Places from './components/place/Places';
-import Contact from './components/homes/Contact';
+import { BrowserRouter } from 'react-router-dom';
 
-import Like from './components/homes/Like';
 
-import PlacesSingle from './components/place/PlacesSingle';
-import Itinerarie from './components/Itinerarie/Itinerarie';
-import ItinerarieView from './components/Itinerarie/ItinerarieView';
-import Login from './components/Profile/Login';
-import Registration from './components/Profile/Registration';
-import ForgotPassword from './components/Profile/ForgotPassword';
-import Profile from './components/Profile/Profile';
-import ChangePassword from './components/Profile/ChangePassword';
-import ResetPassword from './components/Profile/ResetPassword';
-import MyMapComponent from './Map/chuan/MyMap';
-import MyGoogleMap from './Map/chuan/MyGoogleMap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-
+  // <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<App />} >
-          {/* index route để nó mặc định trang đầu */}
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="places" element={<Places />} />
-        
-          {/*  path - endpoint có tham số route param */}
-          {/* <Route path="detail/:placeId" element={<PlacesSingle/>} />   */}
-          <Route path="detail" element={<PlacesSingle/>} />  
-
-          {/* // route param */}
-
-          <Route path="itinerarie" element={<Itinerarie />} />
-          <Route path="itinerarieView" element={<ItinerarieView />} />
-          <Route path="like" element={<Like />} />
-
-          <Route path="contact" element={<Contact />} />
-
-
-
-
-          <Route path="login" element={<Login />} />
-          <Route path="registration" element={<Registration />} />
-
-          <Route path="profile" element={<Profile />} />
-          <Route path="changePassword/:userId" element={<ChangePassword />} />
-          <Route path="forgotPassword" element={<ForgotPassword />} />
-          <Route path="resetPassword" element={<ResetPassword />} />
-         
-
-
-
-          {/* <Route path="Map" element={<MyMapComponent />} /> */}
-          {/* <Route path="Map" element={<MyGoogleMap />} /> */}
-
-          {/* <Route path="PhanTrang" element={<PhanTrang />} /> */}
-
-        </Route>
-
-
-
-      </Routes>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode> 
 );
 
 // If you want to start measuring performance in your app, pass a function
